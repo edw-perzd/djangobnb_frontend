769896db4162939de0cbebcbec30a5cd87db2270
Eduardo <legna246.dav@gmail.com>
2025-03-14 22:44:33 -0600

    Detail page for properties

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,7 +1,11 @@
 import ReservationSidebar from "@/app/components/properties/ReservationSidebar"
 import Image from "next/image"
 
-const PropertyDetailPage = () => {
+import apiService from "@/app/services/apiService"
+
+const PropertyDetailPage = async ({params}: { params: { id: string } }) => {
+    const property = await apiService.get(`/api/properties/${params.id}`)
+
     return(
         <div className="max-w-[1500px] mx-auto px-6">
             <div className="w-full h-[64vh] mb-4 overflow-hidden rounded-xl relative">
@@ -15,29 +19,33 @@ const PropertyDetailPage = () => {
 
             <div className="grid grid-cols-1 md:grid-cols-5 gap-4">
                 <div className="py-6 pr-6 col-span-3">
-                    <h1 className="mb-4 text-4xl">Property name</h1>
+                    <h1 className="mb-4 text-4xl">{property.title}</h1>
                     <span className="mb-6 block text-lg text-gray-600">
-                        4 guest - 2 bedrooms - 1 badroom
+                        {property.guests} guest - {property.bedrooms} bedrooms - {property.bathrooms} badroom
                     </span>
                     <hr />
                     <div className="py-6 flex items-center space-x-4">
-                        <Image 
-                            src="/profile_pic_1.jpg"
-                            width={50}
-                            height={50}
-                            className="rounded-full"
-                            alt="The user name"
-                        />
-                        <p><strong>Edd Perz</strong> is your host</p>
+                        {property.landlord.avatar_url && (
+                            <Image 
+                                src={property.landlord.avatar_url}
+                                width={50}
+                                height={50}
+                                className="rounded-full"
+                                alt="The user name"
+                            />
+                        )}
+                        <p><strong>{property.landlord.name}</strong> is your host</p>
                     </div>
 
                     <hr />
 
                     <p className="mt-6 text-lg">
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus unde aliquid ex totam doloremque nesciunt aliquam, placeat consequatur numquam incidunt officiis necessitatibus repellat atque possimus explicabo eaque. Eveniet, asperiores tenetur.
+                        {property.description}
                     </p>
                 </div>
-                <ReservationSidebar />
+                <ReservationSidebar 
+                    property={property}
+                />
             </div>
         </div>
     )

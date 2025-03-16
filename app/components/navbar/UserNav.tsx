4d8ee51e3d55ad04bb87bcf41b98f6e5d2d5dd56
Eduardo <legna246.dav@gmail.com>
2025-03-15 20:16:06 -0600

    My properties dynamic

--- app/components/navbar/UserNav.tsx
+++ app/components/navbar/UserNav.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { useRouter } from "next/navigation"
 import { useState } from "react"
 import MenuLink from "./MenuLink"
 import LogoutButton from "../LogoutButton"
@@ -14,6 +15,7 @@ interface UserNavProps {
 const UserNav: React.FC<UserNavProps> = ({
     userId
 }) => {
+    const router = useRouter()
     const loginModal = useLoginModal()
     const signupModal = useSignupModal()
     const [isOpen, setIsOpen] = useState(false)
@@ -37,7 +39,17 @@ const UserNav: React.FC<UserNavProps> = ({
             {isOpen && (
                 <div className="w-[220px] absolute top-[60px] right-0 bg-white border rounded-xl shadow-md flex flex-col cursor-pointer">
                     {userId ? (
-                        <LogoutButton />
+                        <>
+                            <MenuLink
+                                label="My properties"
+                                onClick={() => {
+                                    setIsOpen(false)
+                                    router.push('/myproperties')
+                                }}
+                            />
+                        
+                            <LogoutButton />
+                        </>
                     ):
                     (
                         <>

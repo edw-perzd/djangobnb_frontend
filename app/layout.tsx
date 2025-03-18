9ab82175c9fc9e6c4d3b17e870b1fe8feb8a55b4
Eduardo <legna246.dav@gmail.com>
2025-03-18 02:46:19 -0600

    Model for search filter - Country Search - Calendar for check in/check out dates

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Navbar from "./components/navbar/Navbar";
 import LoginModal from "./components/modals/LoginModal";
 import SignupModal from "./components/modals/SignupModal";
 import AddPropertyModal from "./components/modals/AddPropertyModal";
+import SearchModal from "./components/modals/SearchModal";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -42,6 +43,7 @@ export default function RootLayout({
         <LoginModal />
         <SignupModal />
         <AddPropertyModal />
+        <SearchModal/>
       </body>
     </html>
   );
